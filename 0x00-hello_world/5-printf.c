d5973fabf1a30408fd2a7b5c59d6a77cd8206ac6
Kingsley David <thefruitfulvine@yahoo.com>
2023-05-20 02:03:20 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints text to screen using
+ * print function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
+
