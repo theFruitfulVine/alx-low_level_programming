1186d7a4d02dbbbb927563d3921b9d6763265399
Kingsley David <thefruitfulvine@yahoo.com>
2023-05-20 01:53:47 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints text to screen using
+ * puts function
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building ");
-	puts("a multilingual puzzle.");
+	puts("Programming is like building a multilingual puzzle");
 	puts("\n");
 	return (0);
 }
+
