d6fd5d71d770e267583a279dc60e2fc61fed803f
Kingsley David <thefruitfulvine@yahoo.com>
2023-05-23 22:17:29 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_isalpha- Entry point
+ *
+ * Description: This program returns 1 if a character is lowercase
+ * otherwise 0 using putchar header prototype
+ *
+ * Return: 1 (Success) or 0 (Failure)
+ *
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
