a8aecdb377c5799b8134ac0f16fd9399ae8fd8e0
Kingsley David <thefruitfulvine@yahoo.com>
2023-05-23 22:30:38 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_isalpha(int c)- Entry point
+ *
+ * Description: This program returns 1 if a character is lowercase
+ * otherwise 0 using putchar header prototype
+ *
+ * Return: 1 (Success) or 0 (Failure)
+ *
+ */
+
+int print_sign(int n)
+{
+
+	n = n % 10;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
