1efa4bce200074cec37836cb736c790be9564229
Kingsley David <thefruitfulvine@yahoo.com>
2023-05-30 14:17:53 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: input parameter
+ *
+ * Description: This program prints the length of a string
+ *
+ * Return: void
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
