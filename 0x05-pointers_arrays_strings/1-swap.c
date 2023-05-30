336c4569b6d6af4daa7f1e62f7696c1a61b843d7
Kingsley David <thefruitfulvine@yahoo.com>
2023-05-30 13:49:03 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Description: This program swaps the values of two integers
+ *
+ * Return: void
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	c = &a;
+
+	a = &b;
+}
+
