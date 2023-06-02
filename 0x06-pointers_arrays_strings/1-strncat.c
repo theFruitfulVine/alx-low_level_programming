d65f5d6273f73978ecec454295635198862e8cfb
Kingsley David <thefruitfulvine@yahoo.com>
2023-06-02 14:06:56 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,15 +14,16 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0', i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
 
-	for (j = 0; j < n && src[j] != 0; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		dest[i + j] = src[j];
 	}
 
-	dest[i] = '\0';
+	dest[i + j] = '\0';
+
 	return (dest);
 }
